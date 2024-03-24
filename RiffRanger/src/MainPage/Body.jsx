40e897ca73ba39810fb8ee93907b42db0f00f1fa
akm019 <99963365+akm019@users.noreply.github.com>
2024-03-24 10:45:41 +0530

    x

--- RiffRanger/src/MainPage/Body.jsx
+++ RiffRanger/src/MainPage/Body.jsx
@@ -6,6 +6,7 @@ export default function Body() {
   const [typedText, setTypedText] = useState('');
 
   const textToType = 'LEARN AND MASTER THE ART OF GUITAR';
+  const textToType2 = 'Songs. Techniques. Rhythm. Games. Chords. Practice Tools. From Beginner to Advanced!'
 
   useEffect(() => {
     const typingDelay = 50; // Adjust the delay between each character typing
@@ -21,6 +22,21 @@ export default function Body() {
     typeText();
     
   }, []);
+  useEffect(() => {
+    const typingDelay = 50; // Adjust the delay between each character typing
+
+    const typeText2 = (text) => {
+      for (let i = 0; i <= text.length; i++) {
+        setTimeout(() => {
+          setTypedText((prevText) => text.slice(0, i));
+        }, (textToType.length + i) * typingDelay);
+      }
+    };
+
+    typeText2(textToType2);
+
+  }, []);
+  
 
   return (
     <div className='Main flex flex-col justify-between'>
@@ -28,7 +44,7 @@ export default function Body() {
       <div className="flex flex-row-reverse justify-between items-center w-full">
         <img src={mainImage} alt="" className="mt-38 px-110 py-20" />
 
-        <div className="text-black ml-10 text-6xl font-semibold">
+        <div className=" main-text text-black ml-10 text-6xl font-semibold">
           {typedText}
         </div>
       </div>
