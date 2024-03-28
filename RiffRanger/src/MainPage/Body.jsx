49d748f19de173b1098e4a503ce3864d7d689a53
akm019 <99963365+akm019@users.noreply.github.com>
2024-03-28 13:31:28 +0530

    navbar responsive

--- RiffRanger/src/MainPage/Body.jsx
+++ RiffRanger/src/MainPage/Body.jsx
@@ -10,40 +10,41 @@ export default function Body() {
   const textToType = 'LEARN AND MASTER THE ART OF GUITAR';
   const textToType2 = 'Songs. Techniques. Rhythm. Games. Chords. Practice Tools. From Beginner to Advanced!'
 
-  useEffect(() => {
-    const typingDelay = 50; // Adjust the delay between each character typing
+  // useEffect(() => {
+  //   const typingDelay = 50; // Adjust the delay between each character typing
 
-    const typeText = () => {
-      for (let i = 0; i <= textToType.length; i++) {
-        setTimeout(() => {
-          setTypedText(textToType.slice(0, i));
-          if (i === textToType.length) {
-            setShowSecondText(true); // Show the second text after the main text is fully typed
-          }
-        }, i * typingDelay);
-      }
-    };
+  //   const typeText = () => {
+  //     for (let i = 0; i <= textToType.length; i++) {
+  //       setTimeout(() => {
+  //         setTypedText(textToType.slice(0, i));
+  //         if (i === textToType.length) {
+  //           setShowSecondText(true); // Show the second text after the main text is fully typed
+  //         }
+  //       }, i * typingDelay);
+  //     }
+  //   };
 
-    typeText();
+  //   typeText();
 
-  }, []);
+  // }, []);
 
   return (
-    <div className='Main flex flex-col justify-between'>
+    <div className='Main float-left'>
 
       <div className="flex flex-row-reverse justify-between items-center w-full">
-        <img src={mainImage} alt="" className="mt-38 px-110 py-20" />
-
-        <div className="main-text text-black ml-10 text-6xl font-semibold">
-          {typedText}
+        <img src={mainImage} alt="" className="mt-38 py-20 " />
+        <div className='ml-10 pt-16'>
+        <div className="main-text text-black  text-6xl font-semibold">
+          {textToType}
         </div>
+        <div className='mt-10 text-2xl'>
+        {textToType2}
       </div>
-
-      {showSecondText && (
-        <div className="second-text text-black mt-6 text-lg font-semibold">
-          {textToType2}
         </div>
-      )}
+
+        
+      </div>
+      
     </div>
   );
 }
--- RiffRanger/src/NavBar.jsx
+++ RiffRanger/src/NavBar.jsx
@@ -1,23 +1,28 @@
 import React, { useState } from 'react';
 import { MdOutlineMenu } from 'react-icons/md';
 import './NavBar.css';
+import { FaBars } from "react-icons/fa";
+import { FaTimes } from "react-icons/fa";
 
 export default function NavBar() {
-  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  // const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-  const toggleMobileMenu = () => {
-    setMobileMenuOpen(!isMobileMenuOpen);
-  };
-
+  // const toggleMobileMenu = () => {
+  //   setMobileMenuOpen(!isMobileMenuOpen);
+  // };
+const [isClicked, setClicked] = useState(false);
+const toggleClicked = () => {
+  setClicked(!isClicked);
+}
   return (
-    <div className='shadow-md text-xl mt-3 flex flex-row justify-between fixed top-0 w-full p-4 z-10'>
-      <div className='text-3xl font-bold flex flex-row items-center'>
-        <img src='' className='w-10' alt='' /> RiffRanger
+    <div className='shadow-md text-xl mt-2 flex justify-between fixed top-0 w-full p-2 z-10'>
+      <div className='text-3xl ml-4 mt-2 font-bold items-center'>
+        <img src='' className='w-6' alt='' /> RiffRanger
       </div>
       
-      <div className='navbar-items flex flex-row gap-4 mt-4 '>
-          <ul className="flex flex-row justify-center items-center gap-3">
-          <li><a href='#'>COURSE</a></li>
+      <div className='navbar-items mt-2 '>
+      <ul className={`text-2xl gap-6 flex justify-center items-center ${isClicked ? 'navbar-items active ' : 'navbar-items'}`}>
+<li className='text-2xl'><a href='#'>COURSES</a></li>
           <li><a href='#'>TOOLS</a></li>
           <li><a href='#'>EXPLORE</a></li>
           <li><a href='#'>COMMUNITY</a></li>
@@ -25,26 +30,20 @@ export default function NavBar() {
         </ul>
       
       </div>
-      <div className='login-button flex gap-4 mt-4'>
+      <div className='login-button gap-6 mt-2 mr-4'>
         <button className='login-btn bg-gradient-to-r from-primary to-secondary hover:from-secondary hover:to-primary text-white'>
           LOG IN
         </button>
-        <button className='login-btn bg-gradient-to-r from-primary to-secondary hover:from-secondary hover:to-primary text-white'>
-          SIGN UP
-        </button>
+        
       </div>
       {/* Mobile Menu */}
-      {isMobileMenuOpen && (
-        <div className=''>
-          <ul className='list-none'>
-            <li><a href='#'>COURSE</a></li>
-            <li><a href='#'>TOOLS</a></li>
-            <li><a href='#'>EXPLORE</a></li>
-            <li><a href='#'>COMMUNITY</a></li>
-            <li><a href='#'>STORE</a></li>
-          </ul>
-        </div>
-      )}
+      {/* {isMobileMenuOpen && ( */}
+      <div className='icons mt-2'>
+        {isClicked ? <FaTimes onClick={toggleClicked} /> : <FaBars onClick={toggleClicked} />}
+      </div>
+
+      
+      {/* )} */}
     </div>
   );
 }
