b5af6e64ee88df5160a02796348a87790fbb04b0
PriyanshuArya678 <105437949+PriyanshuArya678@users.noreply.github.com>
2024-04-03 18:45:37 +0530

    SoundGuessingGame

--- RiffRanger/src/ChordGuessingGame/GuitarChordSound.jsx
+++ RiffRanger/src/ChordGuessingGame/GuitarChordSound.jsx
@@ -1,22 +1,60 @@
-import React, { useEffect } from 'react'
-import * as Tone from 'tone'
+import React, { useEffect, useState } from 'react';
+import * as Tone from 'tone';
+import axios from 'axios';
+
 const GuitarChordSound = ({ chord }) => {
-  async function playChord(){
+  const stringNotes = [
+    ['E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#'],
+    ['B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#'],
+    ['G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#'],
+    ['D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#'],
+    ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'],
+    ['E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#']
+  ];
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    async function getNotes() {
+      const chordName = chord[0] + '_' + chord.slice(1);
+      const response = await axios.get(`https://api.uberchord.com/v1/chords/${chordName}`);
+      if (response.status === 200) {
+        const data = response.data;
+        const fretData = data[0]['strings'];
+        var string = 1;
+        var noteData = [];
+        for (let i = fretData.length - 1; i >= 0; i--) {
+          if (fretData[i] === 'X') {
+            string++;
+            continue;
+          }
+          if (fretData[i] === ' ') continue;
+          const fretNumber = Number(fretData[i]);
+          const note = stringNotes[string - 1][fretNumber] + '3';
+          noteData.push(note);
+          string++;
+        }
+        setNotes(noteData);
+      }
+    }
+    getNotes();
+  }, [chord]);
+
+  async function playChord() {
     await Tone.start();
     const synth = new Tone.PolySynth(Tone.Synth).toDestination();
-    const now = Tone.now()
-    synth.triggerAttack("C4", now);
-    synth.triggerAttack("E4", now+0.02);
-    synth.triggerAttack("C4", now +0.04);
-    synth.triggerAttack("G4", now +0.06);
-    synth.triggerAttack("E4", now+0.08 );
-    synth.triggerRelease(["C4", "E4", "C4", "G4", "E4"], now + 1);
+    const now = Tone.now();
+    var k = 0;
+    notes.forEach((val) => {
+      synth.triggerAttack(val, now + (k * 0.02));
+      k++;
+    });
+    synth.triggerRelease(notes, now + 1);
   }
+
   return (
     <div>
-      <button onClick={playChord}>play</button>
+      <button className='text-2xl font-bold bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded my-5' onClick={playChord}>Play</button>
     </div>
-    
   );
 };
 
